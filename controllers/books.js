02cffa6849ad67d81cc5a5c9ee19af0ae46e0af2
bcarpenter7 <bennett.carpenter7@gmail.com>
2023-06-20 10:38:31 -0400

    controller

--- controllers/books.js
+++ controllers/books.js
@@ -1,6 +1,14 @@
+const Book = require("../models/book")
 
 
 module.exports = {
     index
 }
 
+async function index(req, res){
+    const booksAll = await Book.find({})
+    const context = {
+        books: booksAll
+    }
+    res.render('books/index', context)
+}
