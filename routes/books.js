e9d96ffad989af10034e8b9167047cdb47cc878a
bcarpenter7 <bennett.carpenter7@gmail.com>
2023-06-20 10:29:04 -0400

    added model

--- routes/books.js
+++ routes/books.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+const bookCtrl = require('../controllers/books')
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
+// GET all/Index
+router.get('/', bookCtrl.index);
+
 
 module.exports = router;
