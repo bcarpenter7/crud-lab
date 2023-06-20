2b6cf911036f30a40180b28ee3e1502af8a6167a
bcarpenter7 <bennett.carpenter7@gmail.com>
2023-06-20 10:17:19 -0400

    more setup

--- server.js
+++ server.js
@@ -4,8 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require('dotenv').config();
+require('./config/database')
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+var methodOverride = require('method-override');
 
 var app = express();
 
